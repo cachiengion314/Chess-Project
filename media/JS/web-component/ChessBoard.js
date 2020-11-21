889e060b513218bc86a6259bb4e4463d9c233ea3
Hoang Nam <cachiengion314@gmail.com>
2020-11-22 02:07:02 +0700

    fix loi kho hieu

--- media/JS/web-component/ChessBoard.js
+++ media/JS/web-component/ChessBoard.js
@@ -2,6 +2,9 @@ class ChessBoard extends HTMLElement {
     constructor() {
         super();
     }
+    connectedCallback() {
+        console.log(`hi chess-board`);
+    }
 }
 
 window.customElements.define(`chess-board`, ChessBoard);
